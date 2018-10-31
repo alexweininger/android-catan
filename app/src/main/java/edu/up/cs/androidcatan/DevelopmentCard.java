931de5775e30c544ce669fb08f009bd1e0e8eb62
alexweininger <alex.weininger@live.com>
2018-10-31 13:31:31 -0700

    useYearOfPlenty

--- app/src/main/java/edu/up/cs/androidcatan/DevelopmentCard.java
+++ app/src/main/java/edu/up/cs/androidcatan/DevelopmentCard.java
@@ -29,7 +29,7 @@ public class DevelopmentCard {
         this.VictoryPoints = VictoryPoints;
     }
 
-    public DevelopmentCard(){
+    public DevelopmentCard() {
         // TODO what is this @Andrew
     }
 
@@ -51,19 +51,19 @@ public class DevelopmentCard {
         player.useDevCard(this);
     }
 
-    public void useKnightCard(Robber robber, Player player){
+    public void useKnightCard(Robber robber, Player player) {
         //TODO: need to get input of tile user pressed on screen to move robber to
         int hexNumber = 5; //placeholder
         robber.setHexagonId(hexNumber);
         player.setArmySize(player.getArmySize() + 1);
     }
 
-    public void useVictoryPointsCard(Player player){
+    public void useVictoryPointsCard(Player player) {
         player.useDevCard(this);
     }
 
-    public void useRoadDevCard(Player player){
-        for(int n = 0; n < 2; n++){
+    public void useRoadDevCard(Player player) {
+        for (int n = 0; n < 2; n++) {
             int startIntersection = 5;//TODO: get the start and end intersection id from users tap
             int endIntersection = 2; //placeholder
             //TODO: need to check if valid intersection
@@ -71,20 +71,32 @@ public class DevelopmentCard {
         }
     }
 
-    public void useYearofPlentyCard(Player player){
-        for (int n = 0; n < 2; n++){
-            int rand = (int) (Math.random() * 5);
-            player.addResources(resources[n], 1);
-        }
+    /**
+     *
+     * @param player
+     */
+    public void useYearofPlentyCard(Player player) {
+        Random random = new Random();
+        player.addResourceCard(random.nextInt(4), 1);
+        player.addResourceCard(random.nextInt(4), 1);
     }
 
-    public void useMonopolyCard(Player player1, Player player2, Player player3, Player player4, String resource){
+    /**
+     *
+     * @param player1
+     * @param player2
+     * @param player3
+     * @param player4
+     * @param resource
+     */
+    public void useMonopolyCard(Player player1, Player player2, Player player3, Player player4, String resource) {
         int totalCollected;
         totalCollected = player2.getResources().get(resource);
         totalCollected = player3.getResources().get(resource);
         totalCollected = player4.getResources().get(resource);
 
         //TODO: Don't know whether this removes all the cards or just one
+
         player2.setResources(resource, player2.getResources().remove(resource));
         player3.setResources(resource, player3.getResources().remove(resource));
         player4.setResources(resource, player4.getResources().remove(resource));
@@ -142,8 +154,6 @@ public class DevelopmentCard {
     //Step 3: For one of the players, have this toString printed to the console
     //as if they selected to play this card (may need a boolean to see if they have
     //the card)
-
-
 
     /**
      * @return string representation of a DevelopmnentCard
