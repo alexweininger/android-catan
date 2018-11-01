fbe62792d6500d892e3a971fe744bd7c47f4d86b
Daniel <danieborg291@gmail.com>
2018-10-31 23:53:46 -0700

    Created action methods and layout for dicard cards due to robber. -db

---
+++ app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanBuildSettlementAcation.java
@@ -0,0 +1,11 @@
+package edu.up.cs.androidcatan.catan.actions;
+
+import edu.up.cs.androidcatan.game.GamePlayer;
+import edu.up.cs.androidcatan.game.actionMsg.GameAction;
+
+public class CatanBuildSettlementAcation extends GameAction {
+    public CatanBuildSettlementAcation(GamePlayer player)
+    {
+        super(player);
+    }
+}
---
+++ app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanTradeWithBankAction.java
@@ -0,0 +1,11 @@
+package edu.up.cs.androidcatan.catan.actions;
+
+import edu.up.cs.androidcatan.game.GamePlayer;
+import edu.up.cs.androidcatan.game.actionMsg.GameAction;
+
+public class CatanTradeWithBankAction extends GameAction {
+    public CatanTradeWithBankAction(GamePlayer player)
+    {
+        super(player);
+    }
+}
