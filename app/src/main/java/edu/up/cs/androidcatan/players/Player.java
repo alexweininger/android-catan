8901c13c716ae31f5262cb1deb5c7e97b218bf4a
Daniel <danieborg291@gmail.com>
2018-10-31 19:23:22 -0700

    Classes created: CatanHumanPlayer CatanComputerPlayer Package Creation: Players -db

--- app/src/main/java/edu/up/cs/androidcatan/players/Player.java
+++ app/src/main/java/edu/up/cs/androidcatan/players/Player.java
@@ -305,7 +305,7 @@ public class Player extends GameHumanPlayer implements OnClickListener {
     /**
      * @return - A random resourceCard is removed from the players inventory and returned.
      */
-    int getRandomCard() {
+    public int getRandomCard() {
 
         if (this.getTotalResourceCardCount() < 1) {
             Log.e(TAG, "getRandomCard: Player does not have any resources cards.");
