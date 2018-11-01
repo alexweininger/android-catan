149feda0c03db370610ee28354514b09309a887e
alexweininger <alex.weininger@live.com>
2018-10-31 19:18:36 -0700

    updated header and date

--- app/src/main/java/edu/up/cs/androidcatan/catan/buildings/Building.java
+++ app/src/main/java/edu/up/cs/androidcatan/catan/buildings/Building.java
@@ -5,10 +5,9 @@ package edu.up.cs.androidcatan.catan.buildings;
  * @author Andrew Lang
  * @author Daniel Borg
  * @author Niraj Mali
- * @version October 24th, 2018
- * https://github.com/alexweininger/game-state
+ * @version October 31, 2018
+ * https://github.com/alexweininger/android-catan
  **/
-
 public abstract class Building {
 
     private int ownerId;
