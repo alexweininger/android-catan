4182e427a052dfda3ebc4aba6040a838a941ec9a
Daniel <danieborg291@gmail.com>
2018-10-31 19:43:38 -0700

    Classes created: CatanHumanPlayer CatanComputerPlayer Package Creation: Players -db

---
+++ app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanTradeAction.java
@@ -0,0 +1,10 @@
+package edu.up.cs.androidcatan.catan.actions;
+
+import edu.up.cs.androidcatan.game.GamePlayer;
+import edu.up.cs.androidcatan.game.actionMsg.GameAction;
+
+public class CatanTradeAction extends GameAction {
+    public CatanTradeAction(GamePlayer player){
+        super(player);
+    }
+}
