d4b3fba2155a700b3b731a4ed36d066acd5b4e39
alexweininger <alex.weininger@live.com>
2018-11-01 21:08:39 -0700

    renamed class

--- app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanTradeWithCustomPortAction.java
+++ app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanTradeWithCustomPortAction.java
@@ -3,13 +3,13 @@ package edu.up.cs.androidcatan.catan.actions;
 import edu.up.cs.androidcatan.game.GamePlayer;
 import edu.up.cs.androidcatan.game.actionMsg.GameAction;
 
-public class CatanTradeWithCustomPort extends GameAction {
+public class CatanTradeWithCustomPortAction extends GameAction {
     /**
      * constructor for GameAction
      *
      * @param player the player who created the action
      */
-    public CatanTradeWithCustomPort(GamePlayer player) {
+    public CatanTradeWithCustomPortAction(GamePlayer player) {
         super(player);
     }
 }
