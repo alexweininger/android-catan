285be3561a1712f69f7eec426d7800919c873181
alexweininger <alex.weininger@live.com>
2018-11-01 20:49:13 -0700

    created constructor in action class

--- app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanTradeWithCustomPort.java
+++ app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanTradeWithCustomPort.java
@@ -1,6 +1,15 @@
 package edu.up.cs.androidcatan.catan.actions;
 
+import edu.up.cs.androidcatan.game.GamePlayer;
 import edu.up.cs.androidcatan.game.actionMsg.GameAction;
 
 public class CatanTradeWithCustomPort extends GameAction {
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public CatanTradeWithCustomPort(GamePlayer player) {
+        super(player);
+    }
 }
