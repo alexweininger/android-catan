cf686aa01ad8585869de2a0f3018c38892aa4499
alexweininger <alex.weininger@live.com>
2018-11-03 01:40:39 -0700

    commented out map logging in de. const.

--- app/src/main/java/edu/up/cs/androidcatan/catan/gamestate/Board.java
+++ app/src/main/java/edu/up/cs/androidcatan/catan/gamestate/Board.java
@@ -97,8 +97,8 @@ public class Board {
         generateHexagonTiles();
 
         // logging
-        Log.i(TAG, "int to hex map: " + this.intToHexIdMap.toString());
-        Log.i(TAG, "hex to int map" + this.hexToIntIdMap.toString());
+//        Log.i(TAG, "int to hex map: " + this.intToHexIdMap.toString());
+//        Log.i(TAG, "hex to int map" + this.hexToIntIdMap.toString());
 
         int desertTileId = 0; // TODO
         robber = new Robber(desertTileId);
