b49818e592fea25c2e95c8802be2a28d93cd7f7e
malin383 <nirajlmali@gmail.com>
2018-11-05 08:54:17 -0800

    test for Board.validBuildingLocation

--- app/src/androidTest/java/catan/tests/ExampleUnitTest.java
+++ app/src/androidTest/java/catan/tests/ExampleUnitTest.java
@@ -2,11 +2,27 @@ package catan.tests;
 
 import android.test.AndroidTestCase;
 
-import org.junit.Test;
+import org.junit.*;
 
-public class ExampleUnitTest extends AndroidTestCase {
+import edu.up.cs.androidcatan.catan.gamestate.Board;
+
+import static junit.framework.Assert.*;
+
+public class ExampleUnitTest{
     @Test
-    public static void example(){
+    public void example(){
         System.out.println("Hello");
+    }
+
+    @Test
+    public void testValidBuildingLocation(){
+        Board board = new Board();
+        assertTrue(board.validBuildingLocation(0, true, 0));
+        assertFalse(board.validBuildingLocation(0, true, -1));
+        assertFalse(board.validBuildingLocation(0, true, 90));
+        assertFalse(board.validBuildingLocation(-3, true, -1));
+        assertFalse(board.validBuildingLocation(-1, true, 5));      //bug
+        assertTrue(board.validBuildingLocation(3, true, 53));
+        assertTrue(board.validBuildingLocation(2, true, 24));
     }
 }
