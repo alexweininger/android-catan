069736fff88f26bf86f8c4d502721a3595906223
alexweininger <alex.weininger@live.com>
2018-11-08 23:01:02 -0800

    removed unused drawing methods from the data hexagon class

--- app/src/main/java/edu/up/cs/androidcatan/catan/gamestate/Hexagon.java
+++ app/src/main/java/edu/up/cs/androidcatan/catan/gamestate/Hexagon.java
@@ -1,7 +1,5 @@
 package edu.up.cs.androidcatan.catan.gamestate;
 
-import android.graphics.Path;
-
 /**
  * @author Alex Weininger
  * @author Andrew Lang
@@ -58,46 +56,6 @@ public class Hexagon {
 
     public int getHexagonId () {
         return this.hexagonId;
-    }
-
-    /**
-     * createHexagonPath() creates a Path object from given hexagon corner x and y values
-     *
-     * @param corners - 2d array of x and y cords for the corners
-     * @return Path
-     */
-    public Path createHexagonPath (int[][] corners) {
-        Path hexagonPath = new Path();
-        hexagonPath.moveTo(corners[0][0], corners[0][1]);
-
-        for (int i = 1; i < corners.length; i++) {
-            hexagonPath.lineTo(corners[i][0], corners[i][1]);
-        }
-        hexagonPath.close();
-
-        return hexagonPath;
-    }
-
-    /**
-     * calculateHexagonPoints() generates an array of points (x, y) for the corners of a hexagon
-     *
-     * @param x - x position
-     * @param y - y position
-     * @param size - size, measured from center to a corner
-     * @return int[][]
-     */
-    public static int[][] calculateHexagonPoints (int x, int y, int size) {
-        int[][] points = new int[6][2];
-        double angle_deg, angle_rad;
-
-        for (int i = 0; i < 6; i++) {
-            angle_deg = 60 * i - 30;
-            angle_rad = Math.PI / 180 * angle_deg;
-
-            points[i][0] = (int) (x + size * Math.cos(angle_rad));
-            points[i][1] = (int) (y + size * Math.sin(angle_rad));
-        }
-        return points;
     }
 
     /**
