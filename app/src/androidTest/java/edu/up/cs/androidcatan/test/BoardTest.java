a702158dab36716b2e798ca9cd1c42c0f65c2889
Andrew <andrewespn@hotmail.com>
2018-11-08 21:05:32 -0800

    Added to testIGraph and moved testHGraph over to testBoard class. Deleted HexagonTest class

--- app/src/androidTest/java/edu/up/cs/androidcatan/test/BoardTest.java
+++ app/src/androidTest/java/edu/up/cs/androidcatan/test/BoardTest.java
@@ -66,6 +66,18 @@ public class BoardTest {
         assertFalse(iGraph[4][18]); //wrong
         assertFalse(iGraph[19][46]); //wrong
         assertTrue(iGraph[49][50]);
-        
+        assertTrue(iGraph[8][29]);
+    }
+
+    @Test
+    public void testHGraph() {
+        Board board = new Board();
+        boolean hGraph[][] = board.getHGraph();
+        assertTrue(hGraph[5][14]);
+        assertTrue(hGraph[5][0]);
+        assertTrue(hGraph[5][14]);
+        assertTrue(hGraph[5][15]);
+        assertTrue(hGraph[5][16]);
+        assertTrue(hGraph[5][6]);
     }
 }
