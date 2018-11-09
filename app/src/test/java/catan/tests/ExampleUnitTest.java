f668f342effd9b244dae0e5796dce3db33de502d
alexweininger <alex.weininger@live.com>
2018-11-08 19:07:10 -0800

    testing changing the NUM TILES array

--- app/src/test/java/catan/tests/ExampleUnitTest.java
+++ app/src/test/java/catan/tests/ExampleUnitTest.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import edu.up.cs.androidcatan.catan.gamestate.Board;
 import edu.up.cs.androidcatan.catan.gamestate.Hexagon;
 
-im
+import static org.junit.Assert.*;
 
 public class ExampleUnitTest {
     @Test
@@ -17,10 +17,13 @@ public class ExampleUnitTest {
 
 
     @Test
-    public static void testGenerateHexagonTiles () {
+    public static void testGenerateHexagonTilesResourceDistribution () {
+
+        int[] resourceTypeCount = {3, 4, 4, 3, 4, 1};
+        int[] chitValuesCount = {1, 0, 1, 2, 2, 2, 2, 0, 2, 2, 2, 2, 1};
+        int[] resources = {0, 1, 2, 3, 4, 5};
 
         Board board = new Board();
-
         ArrayList<Hexagon> hexes = board.getHexagons();
 
         // count the amount that each resource type appears on the board
@@ -34,7 +37,14 @@ public class ExampleUnitTest {
             assert i <= 4;
         }
 
+        assertEquals(count[0], 0);
+
         assert true;
+    }
+
+    @Test
+    public static void testGenerateHexagonTilesChitDistribution() {
+
     }
 
 }
