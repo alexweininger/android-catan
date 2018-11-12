07bb1182b98b5f9c6f050f48c8ab133fc358dfc0
Andrew <andrewespn@hotmail.com>
2018-11-11 19:36:48 -0800

    Fixed constructor within CatanUseDevCardAction

--- app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanUseDevCardAction.java
+++ app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanUseDevCardAction.java
@@ -13,11 +13,13 @@ import edu.up.cs.androidcatan.game.actionMsg.GameAction;
  * https://github.com/alexweininger/android-catan
  **/
 public class CatanUseDevCardAction extends GameAction {
+    private static final String TAG = "CatanUseDevCardAction";
     private int devCardId;
 
     public CatanUseDevCardAction(GamePlayer player, int devCardId){
         super(player);
-        //Log.d(TAG, "CatanUseDevCardAction: ");
+        Log.d(TAG, "CatanUseDevCardAction: called with player = [" + player + "], devCardId = [" + devCardId + "]");
+        this.devCardId = devCardId;
     }
 
 }
