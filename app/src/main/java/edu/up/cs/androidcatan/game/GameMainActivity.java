488cb41fdebb962a712c9bf917f43a0542168166
alexweininger <alex.weininger@live.com>
2018-11-12 02:00:45 -0800

    Merge branch 'master-clean' into alex-clean-2
    # Conflicts:
    #	app/src/main/java/edu/up/cs/androidcatan/catan/CatanGameState.java
    #	app/src/main/java/edu/up/cs/androidcatan/catan/CatanHumanPlayer.java
    #	app/src/main/java/edu/up/cs/androidcatan/catan/graphics/HexagonDrawable.java

--- app/src/main/java/edu/up/cs/androidcatan/game/GameMainActivity.java
+++ app/src/main/java/edu/up/cs/androidcatan/game/GameMainActivity.java
@@ -456,10 +456,10 @@ View.OnClickListener {
 		v.setOnClickListener(this);
 
 
-		String ipCode = IPCoder.encodeLocalIP();
-		String ipAddress = IPCoder.getLocalIpAddress();
-		TextView ipText = findViewById(R.id.ipCodeLabel);
-		ipText.setText(ipText.getText()+ipCode+" ("+ipAddress+") ");
+//		String ipCode = IPCoder.encodeLocalIP();
+//		String ipAddress = IPCoder.getLocalIpAddress();
+//		TextView ipText = findViewById(R.id.ipCodeLabel);
+//		ipText.setText(ipText.getText()+ipCode+" ("+ipAddress+") ");
 
 	}// initStarterGui
 
