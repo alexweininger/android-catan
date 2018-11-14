b1d7209eea29a81999df8b2359ac0f02a7c23d8e
Daniel <danielborg291@gmail.com>
2018-11-13 19:46:39 -0800

    fixed array out of bound for drawing city pictures -db

--- app/src/main/java/edu/up/cs/androidcatan/catan/CatanGameState.java
+++ app/src/main/java/edu/up/cs/androidcatan/catan/CatanGameState.java
@@ -353,7 +353,7 @@ public class CatanGameState extends GameState{
         if (this.currentDiceSum == 7) {
             // todo activate robber
             Log.i(TAG, "rollDice: The robber has been activated.");
-            this.isRobberPhase = true;
+            //this.isRobberPhase = true;
         } else {
             Log.i(TAG, "rollDice: Calling the produceResources method.");
             produceResources(this.currentDiceSum);
