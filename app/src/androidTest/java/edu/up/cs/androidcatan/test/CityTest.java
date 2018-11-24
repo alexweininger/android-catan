25f91baf421984b8db8b0e88d3fe926f6b49e95a
Daniel <danielborg291@gmail.com>
2018-11-24 14:09:22 -0800

    JUnit Tests
    -BuildingTest
    -CityTest
    -DevelopmentCardTest
    -HexagonTest
    -PortTest
    -RoadTest
    -SettlementTest
    ---------------
    -Made Port constructor public to be used in test case
    
    -db

---
+++ app/src/androidTest/java/edu/up/cs/androidcatan/test/CityTest.java
@@ -0,0 +1,17 @@
+package edu.up.cs.androidcatan.test;
+
+import org.junit.Test;
+
+import edu.up.cs.androidcatan.catan.gamestate.buildings.City;
+
+import static org.junit.Assert.assertEquals;
+
+public class CityTest {
+
+    @Test
+    public void getVictoryPoints()
+    {
+        City city = new City(0);
+        assertEquals(2, city.getVictoryPoints());
+    }
+}
---
+++ app/src/androidTest/java/edu/up/cs/androidcatan/test/DevelopmentCardTest.java
@@ -0,0 +1,35 @@
+package edu.up.cs.androidcatan.test;
+
+import org.junit.Test;
+
+import edu.up.cs.androidcatan.catan.gamestate.DevelopmentCard;
+
+import static org.junit.Assert.assertEquals;
+
+public class DevelopmentCardTest {
+
+    @Test
+    public void setPlayable()
+    {
+        DevelopmentCard developmentCard = new DevelopmentCard(2);
+        assertEquals(false, developmentCard.isPlayable());
+
+        developmentCard.setPlayable(true);
+        assertEquals(true, developmentCard.isPlayable());
+
+        developmentCard.setPlayable(false);
+        assertEquals(false, developmentCard.isPlayable());
+    }
+
+    @Test
+    public void setDevCardId()
+    {
+        DevelopmentCard developmentCard = new DevelopmentCard(2);
+        assertEquals(2, developmentCard.getDevCardId());
+
+        developmentCard.setDevCardId(5);
+        assertEquals(5, developmentCard.getDevCardId());
+    }
+
+
+}
