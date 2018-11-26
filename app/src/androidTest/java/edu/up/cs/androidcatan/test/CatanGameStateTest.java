17048fa0533e34ec877df81e91035ed3fccf4bfb
Daniel <danielborg291@gmail.com>
2018-11-25 23:23:10 -0800

    JUnit Test
    -CatanGameState
    ---------------
    
    -db

---
+++ app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanGameStateTest.java
@@ -0,0 +1,13 @@
+package edu.up.cs.androidcatan.test;
+
+import org.junit.Test;
+
+import edu.up.cs.androidcatan.catan.gamestate.buildings.City;
+
+import static org.junit.Assert.assertEquals;
+
+public class CatanGameStateTest {
+
+    @Test
+
+}
