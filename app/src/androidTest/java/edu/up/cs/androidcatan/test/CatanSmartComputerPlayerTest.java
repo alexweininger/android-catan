be52230b88f7a688d49d6f2887bc9c9ff37b130b
Niraj Mali <nirajlmali@gmail.com>
2018-11-27 01:11:30 -0800

    Added stubs for Unit test methods for CSCPT

--- app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanSmartComputerPlayerTest.java
+++ app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanSmartComputerPlayerTest.java
@@ -1,5 +1,33 @@
 package edu.up.cs.androidcatan.test;
 
+import android.util.Log;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import edu.up.cs.androidcatan.catan.CatanGameState;
+import edu.up.cs.androidcatan.catan.gamestate.buildings.Road;
+
 public class CatanSmartComputerPlayerTest {
 
+    @Test
+    //by Niraj Mali
+    private void testTryMoveRobber(){}
+
+    @Test
+    //by Niraj Mali
+    private void testGetBuildingOfPlayer(){}
+
+    @Test
+    //by Niraj Mali
+    private void testGetPlayerRoads() {}
+
+    @Test
+    //by Niraj Mali
+    private void testGetPlayerRoadIntersection(){}
+
+    @Test
+    //by Niraj Mali
+    private void testCheckIntersectionResource(){}
 }
