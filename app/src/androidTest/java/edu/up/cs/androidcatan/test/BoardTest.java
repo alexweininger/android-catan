4c1c68f354b487b147e384ebdc8ba67101951c1f
Andrew <andrewespn@hotmail.com>
2018-11-26 21:11:37 -0800

    testValidCityLocation

--- app/src/androidTest/java/edu/up/cs/androidcatan/test/BoardTest.java
+++ app/src/androidTest/java/edu/up/cs/androidcatan/test/BoardTest.java
@@ -168,5 +168,11 @@ public class BoardTest {
     public void testValidCityLocation(){
         Board board = new Board();
 
+        assertFalse(board.validCityLocation(1,-3));
+        assertFalse(board.validCityLocation(-1, 3));
+        assertFalse(board.validCityLocation(-1,-4));
+
+        board.getBuildings()[0] = new Settlement(1);
+        assertTrue(board.validCityLocation(1, 0));
     }
 }
