7135123cbea4cef6460cbab4a78aaa7030b6cbaa
alexweininger <alex.weininger@live.com>
2018-11-27 01:23:33 -0800

    formatting and typo fix

--- app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanHumanPlayerTest.java
+++ app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanHumanPlayerTest.java
@@ -9,13 +9,12 @@ import static junit.framework.TestCase.assertTrue;
 public class CatanHumanPlayerTest {
 
     @Test //Written By: Niraj
-    public void tryBuildRoad()
-    {
+    public void tryBuildRoad () {
         CatanGameState state = new CatanGameState();
-        state.setSetupPhase(true);;
+        state.setSetupPhase(true);
         state.setCurrentPlayerId(0);
         state.getPlayerList().get(0).addResourceCard(0, 1);
-        state.getPlayerList().get(0).addResourceCard(2,1);
+        state.getPlayerList().get(0).addResourceCard(2, 1);
 
         assertTrue(state.isSetupPhase());
     }
