e90cba39ae336458d03b204b85bb60f6a5d66063
Daniel <danielborg291@gmail.com>
2018-11-26 21:28:40 -0800

    Unit Tests
    -CatamHumanPlayerTest
    -------------
    
    -db

--- app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanHumanPlayerTest.java
+++ app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanHumanPlayerTest.java
@@ -2,9 +2,8 @@ package edu.up.cs.androidcatan.test;
 
 import org.junit.Test;
 
-import edu.up.cs.androidcatan.catan.CatanHumanPlayer;
+import edu.up.cs.androidcatan.catan.CatanGameState;
 
-import static junit.framework.Assert.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 public class CatanHumanPlayerTest {
@@ -12,12 +11,13 @@ public class CatanHumanPlayerTest {
     @Test
     public void tryBuildRoad()
     {
-        CatanHumanPlayer catanHumanPlayer = new CatanHumanPlayer("Name");
-        catanHumanPlayer.state.setSetupPhase(true);;
-        catanHumanPlayer.state.setCurrentPlayerId(0);
-        catanHumanPlayer.state.getPlayerList().get(0).addResourceCard(0, 1);
-        catanHumanPlayer.state.getPlayerList().get(0).addResourceCard(2,1);
+        CatanGameState state = new CatanGameState();
+        state.setSetupPhase(true);;
+        state.setCurrentPlayerId(0);
+        state.getPlayerList().get(0).addResourceCard(0, 1);
+        state.getPlayerList().get(0).addResourceCard(2,1);
 
+        assertTrue(state.isSetupPhase());
     }
 
 }
