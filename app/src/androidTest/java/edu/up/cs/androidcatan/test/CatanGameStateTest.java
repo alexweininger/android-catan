2449c7db3f5f0bafd30d9065394b3f7c0f5c2a85
Niraj Mali <nirajlmali@gmail.com>
2018-11-26 23:53:05 -0800

    Tests for setRobberPhase() and checkPlayerResources()(needs more) are implemented

--- app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanGameStateTest.java
+++ app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanGameStateTest.java
@@ -4,21 +4,34 @@ import android.util.Log;
 
 import org.junit.Test;
 
+import edu.up.cs.androidcatan.catan.CatanGameState;
 import edu.up.cs.androidcatan.catan.gamestate.buildings.City;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class CatanGameStateTest {
 
     /*----------------------------------------Robber Methods------------------------------------------*/
     @Test
-    public void testSetRobberPhase() {}
+    public void testSetRobberPhase() {
+        CatanGameState state = new CatanGameState();
+        state.setRobberPhase(true);
+        assertTrue(state.getRobberPhase());
+        assertFalse(!state.getRobberPhase());
+        state.setRobberPhase(false);
+        assertTrue(!state.getRobberPhase());
+        assertFalse(state.getRobberPhase());
+    }
 
     @Test
-    public void testGetRobberPhase() {}
-
-    @Test
-    public void testCheckPlayerResources(){}
+    public void testCheckPlayerResources(){
+        CatanGameState state = new CatanGameState();
+        assertFalse(state.checkPlayerResources(0));
+        assertTrue(state.getRobberPlayerListHasDiscarded()[0]);
+        state.setRobberPlayerListHasDiscarded(new boolean[]{true, true, true, true});
+        assertFalse(state.checkPlayerResources(0));
+    }
 
     @Test
     public void testValidDiscard(){}
