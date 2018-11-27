785d23d28f31ca791b3ea49744730b22bdc891c1
Niraj Mali <nirajlmali@gmail.com>
2018-11-26 23:47:31 -0800

    Added stubs in CGameStateTest for robber method tests

--- app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanGameStateTest.java
+++ app/src/androidTest/java/edu/up/cs/androidcatan/test/CatanGameStateTest.java
@@ -1,5 +1,7 @@
 package edu.up.cs.androidcatan.test;
 
+import android.util.Log;
+
 import org.junit.Test;
 
 import edu.up.cs.androidcatan.catan.gamestate.buildings.City;
@@ -7,6 +9,31 @@ import edu.up.cs.androidcatan.catan.gamestate.buildings.City;
 import static org.junit.Assert.assertEquals;
 
 public class CatanGameStateTest {
+
+    /*----------------------------------------Robber Methods------------------------------------------*/
+    @Test
+    public void testSetRobberPhase() {}
+
+    @Test
+    public void testGetRobberPhase() {}
+
+    @Test
+    public void testCheckPlayerResources(){}
+
+    @Test
+    public void testValidDiscard(){}
+
+    @Test
+    public void testDiscardResources(){}
+
+    @Test
+    public void testAllPlayersHaveDiscarded(){}
+
+    @Test
+    public void testMoveRobber(){}
+
+    @Test
+    public void testRobberSteal() {}
 
 
 }
