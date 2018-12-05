4a2cebf1fff736f093fbd9dee3e0cac499c0ea77
Daniel <danielborg291@gmail.com>
2018-12-04 22:50:42 -0800

    added JavaDoc comments to CatanUseMonopolyCardAction -db

--- app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanUseMonopolyCardAction.java
+++ app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanUseMonopolyCardAction.java
@@ -9,6 +9,11 @@ public class CatanUseMonopolyCardAction extends GameAction {
     private static final String TAG = "CatanUseMonopolyCardAction";
     private int chosenResource;
 
+    /**
+     * CatanUseMonopolyCardAction constructor
+     * @param player the player calling the action
+     * @param chosenResource the resource that the player chose
+     */
     public CatanUseMonopolyCardAction(GamePlayer player, int chosenResource){
         super(player);
         this.chosenResource = chosenResource;
