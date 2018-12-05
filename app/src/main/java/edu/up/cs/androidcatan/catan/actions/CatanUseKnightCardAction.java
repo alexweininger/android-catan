03c49350b87babd9499b970fbd20c6fd33c6f02c
Daniel <danielborg291@gmail.com>
2018-12-04 22:49:21 -0800

    added JavaDoc comments to CatanUseKnightCardAction -db

--- app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanUseKnightCardAction.java
+++ app/src/main/java/edu/up/cs/androidcatan/catan/actions/CatanUseKnightCardAction.java
@@ -8,6 +8,10 @@ import edu.up.cs.androidcatan.game.actionMsg.GameAction;
 public class CatanUseKnightCardAction extends GameAction {
     private static final String TAG = "CatanUseKnightCardAction";
 
+    /**
+     * CatanUseKnightCardAction
+     * @param player the player calling the action
+     */
     public CatanUseKnightCardAction(GamePlayer player){
         super(player);
         Log.d(TAG, "CatanUseKnightCardAction called");
